d3154bd2f21df93b9bf001e48b69dc7c90956717
remimoul <neptunes92390@outlook.fr>
2025-02-26 12:28:04 +0100

    Update app.ts to demonstrate usage of premierElement with different types

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import { premierElement } from "./s2/ep1/generics";
 
-const nombres = [1, 2, 3];
-console.log("tableau nombres : ", premierElement(nombres));
+console.log(
+  "tableau string : ",
+  premierElement<string>(["Jean", "Jacques", "Pierre"])
+);
 
-const prenoms = ["Jean", "Jacques", "Pierre"];
-console.log("tableau string : ", premierElement(prenoms));
+console.log("tableau number : ", premierElement<number>([1, 2, 3, 4, 5]));
 
-const objets = [
-  { id: 1, name: "modibo" },
-  { id: 2, name: "jean" },
-  { id: 3, name: "pierre" },
-];
-console.log("tableau objets : ", premierElement(objets));
+console.log(
+  "tableau objets : ",
+  premierElement<object>([
+    { id: 1, name: "modibo" },
+    { id: 2, name: "jean" },
+    { id: 3, name: "pierre" },
+  ])
+);
